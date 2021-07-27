7eb3a062934bbd547858c6f9f35976be47d98a12
eduardodcs <eddcesilva@gmail.com>
2021-07-26 23:26:43 -0300

    Criando Repositório

---
+++ src/main/java/br/com/eduardo/produtividade/ProdutividadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.produtividade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutividadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutividadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eduardo/produtividade/ProdutividadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.produtividade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutividadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
